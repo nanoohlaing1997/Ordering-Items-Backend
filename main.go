f22d00d7a7d6f28c328c815c9e7bccab8d7e536d
Nan Oo Hlaing <nanoohlaing237@gmail.com>
2023-01-10 14:24:18 +0630

    go-order-item-small-project

--- main.go
+++ main.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"noh/go-order-items/cmd"
 	"noh/go-order-items/controller"
-	"noh/go-order-items/database"
 	"os"
 
 	"github.com/gorilla/mux"
@@ -15,40 +13,6 @@ import (
 
 func main() {
 	fmt.Println("hello from Ordering Items Project")
-	cmd.Execute()
-	godotenv.Load()
-	// dbName := os.Getenv("GO_ORDER")
-
-	// connection := dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// itemCategory := &models.ItemCategory{
-	// 	Name: "medicine",
-	// }
-	// category, _ := models.ItemCategoryCreate(db, itemCategory)
-
-	// item := &models.Item{
-	// 	Name:       "Sayar Kho",
-	// 	Price:      "1000",
-	// 	CategoryID: category.ID,
-	// }
-
-	// itemDetail, _ := models.ItemCreate(db, item)
-
-	// customerOrder := &models.CustomerOrder{
-	// 	CustomerId: 1,
-	// 	Attribute:  "items_id",
-	// 	Value:      itemDetail.ID,
-	// }
-
-	// order, _ := models.CustomerOrderCreate(db, customerOrder)
-	// fmt.Println("Done process.........................")
-	// fmt.Println(*order)
-
-	database.DBManger()
 
 	// Initialize the router
 	router := mux.NewRouter().StrictSlash(true)
@@ -57,6 +21,7 @@ func main() {
 	RegisterProductRoutes(router)
 
 	// Start the server
+	godotenv.Load()
 	log.Println(fmt.Sprintf("Starting Server on port %s", os.Getenv("PORT")))
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", os.Getenv("PORT")), router))
 }
